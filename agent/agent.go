56105952adc6e46e8e051ad1ee0ae19889e241d8
Fabio Tavares Pereira Rego <fabiotavarespr@gmail.com>
2020-06-16 13:47:13 -0300

    init sysclock in initfromagentbuild methed

--- agent/agent.go
+++ agent/agent.go
@@ -1,7 +1,6 @@
 package agent
 
 import (
-	"fmt"
 	"net/http"
 	"sync"
 	"time"
@@ -11,10 +10,10 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/labbsr0x/bindman-dns-file-agent/agent/config"
 	"github.com/labbsr0x/bindman-dns-file-agent/file"
-	"github.com/robfig/cron/v3"
 	"github.com/sirupsen/logrus"
 )
 
+// Agent struct owns items needed to start the service
 type Agent struct {
 	*config.AgentBuilder
 	file          file.File
@@ -28,6 +27,8 @@ func (a *Agent) InitFromAgentBuilder(agentBuilder *config.AgentBuilder) *Agent {
 	a.AgentBuilder = agentBuilder
 	a.app = gin.Default()
 	a.file = file.GetFile(a.AgentConfigPath)
+	a.SyncLock = new(sync.RWMutex)
+	// a.WebhookClient = hook.New(new(hook.BindmanHTTPHelper))
 
 	logLevel, err := logrus.ParseLevel(a.AgentBuilder.LogLevel)
 	if err != nil {
@@ -39,6 +40,7 @@ func (a *Agent) InitFromAgentBuilder(agentBuilder *config.AgentBuilder) *Agent {
 	return a
 }
 
+// Run a Server instance
 func (a *Agent) Run() {
 	consumerGroup := a.app.Group("/")
 	{
@@ -49,15 +51,6 @@ func (a *Agent) Run() {
 
 func index(ctx *gin.Context) {
 	ctx.String(http.StatusOK, "Welcome to Bindman DNS File Agent")
-}
-
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
-func startSync() {
-	c := cron.New(cron.WithSeconds())
-	c.AddFunc("0 * * * * *", func() { fmt.Println("Every minute") })
-	c.AddFunc("*/5 * * * * *", func() { fmt.Println("Every 5 second") })
-	c.Start()
 }
 
 // Sync defines a routine for syncing the dns records present in the docker swarm and being managed by the bindman dns manager
@@ -73,14 +66,16 @@ func (a *Agent) Sync() {
 			for _, record := range a.file.Records {
 				logrus.Infof("%v", record)
 
-				bs, err := a.WebhookClient.GetRecord(record.Name, "A")
-				if err != nil { // means record was not found on manager; so we create it
-					a.delegate("create", a.file.Domain, record)
-				}
+				// bs, err := a.WebhookClient.GetRecord(record.Name, "A")
+				// if err != nil { // means record was not found on manager; so we create it
+				// a.delegate("create", a.file.Domain, record)
+				logrus.Infof("Create %v", a.file.Domain)
+				// }
 
-				if bs.Name != record.Name || bs.Value != a.DNSReverseProxyAddr || bs.Type != "A" { // if true, record exists and needs to be update
-					a.delegate("update", a.file.Domain, record)
-				}
+				// if bs.Name != record.Name || bs.Value != a.DNSReverseProxyAddr || bs.Type != "A" { // if true, record exists and needs to be update
+				// a.delegate("update", a.file.Domain, record)
+				logrus.Infof("Update %v", a.file.Domain)
+				// }
 			}
 		}()
 		backoffWait(maxTries, leftTries, time.Minute) // wait time increases exponentially
@@ -89,20 +84,20 @@ func (a *Agent) Sync() {
 }
 
 // delegate appropriately calls the dns manager to handle the addition or removal of a DNS rule
-func (a *Agent) delegate(action string, domain string, record file.Records) {
-	var ok bool
-	var err error
+// func (a *Agent) delegate(action string, domain string, record file.Records) {
+// 	var ok bool
+// 	var err error
 
-	if action == "update" {
-		ok, err = a.WebhookClient.UpdateRecord(&hookTypes.DNSRecord{Name: record.Name, Type: record.Type, Value: record.Value})
-	}
+// 	if action == "update" {
+// 		ok, err = a.WebhookClient.UpdateRecord(&hookTypes.DNSRecord{Name: record.Name, Type: record.Type, Value: record.Value})
+// 	}
 
-	if action == "create" {
-		ok, err = a.WebhookClient.AddRecord(record.Name, record.Type, record.Value) // adds to the dns manager
-	}
+// 	if action == "create" {
+// 		ok, err = a.WebhookClient.AddRecord(record.Name, record.Type, record.Value) // adds to the dns manager
+// 	}
 
-	if !ok {
-		logrus.Errorf("Error to %v the Domain '%v' from the service '%v': %v", action, domain, record.Name, err)
-	}
+// 	if !ok {
+// 		logrus.Errorf("Error to %v the Domain '%v' from the service '%v': %v", action, domain, record.Name, err)
+// 	}
 
-}
+// }
