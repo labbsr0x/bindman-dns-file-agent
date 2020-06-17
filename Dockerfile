3541588150f656b25f26a0663bc283b64ae0afdf
Fabio Tavares Pereira Rego <fabiotavarespr@gmail.com>
2020-06-17 15:05:00 -0300

    Finishing happy path

--- Dockerfile
+++ Dockerfile
@@ -12,13 +12,16 @@ RUN CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o /bindman-dns-file
 
 FROM alpine
 
-RUN apk update \
+RUN mkdir /data \
+    && apk update \
     && apk add --no-cache ca-certificates \
     && update-ca-certificates
 
 ENV BINDMAN_DNS_MANAGER_ADDR=""
 ENV BINDMAN_DNS_REVERSE_PROXY_ADDR=""
-ENV BINDMAN_DNS_CONFIG_FILE=""
+ENV BINDMAN_AGENT_CONFIG_FILE=""
+ENV BINDMAN_LOG_LEVEL=""
+ENV BINDMAN_PORT=""
 
 COPY --from=builder /bindman-dns-file-agent /
 
--- agent/agent.go
+++ agent/agent.go
@@ -6,6 +6,7 @@ import (
 	"time"
 
 	hook "github.com/labbsr0x/bindman-dns-webhook/src/client"
+	hookTypes "github.com/labbsr0x/bindman-dns-webhook/src/types"
 
 	"github.com/gin-gonic/gin"
 	"github.com/labbsr0x/bindman-dns-file-agent/agent/config"
@@ -28,7 +29,12 @@ func (a *Agent) InitFromAgentBuilder(agentBuilder *config.AgentBuilder) *Agent {
 	a.app = gin.Default()
 	a.file = file.GetFile(a.AgentConfigPath)
 	a.SyncLock = new(sync.RWMutex)
-	// a.WebhookClient = hook.New(new(hook.BindmanHTTPHelper))
+	webhookClient, err := hook.New(a.DNSManagerAddr, http.DefaultClient)
+	if err != nil {
+		logrus.Errorf("Not able to parse log level string. Setting default level: info.")
+	}
+
+	a.WebhookClient = webhookClient
 
 	logLevel, err := logrus.ParseLevel(a.AgentBuilder.LogLevel)
 	if err != nil {
@@ -66,16 +72,14 @@ func (a *Agent) Sync() {
 			for _, record := range a.file.Records {
 				logrus.Infof("%v", record)
 
-				// bs, err := a.WebhookClient.GetRecord(record.Name, "A")
-				// if err != nil { // means record was not found on manager; so we create it
-				// a.delegate("create", a.file.Domain, record)
-				logrus.Infof("Create %v", a.file.Domain)
-				// }
+				bs, err := a.WebhookClient.GetRecord(record.Name, "A")
+				if err != nil { // means record was not found on manager; so we create it
+					a.delegate("create", a.file.Domain, record)
+				}
 
-				// if bs.Name != record.Name || bs.Value != a.DNSReverseProxyAddr || bs.Type != "A" { // if true, record exists and needs to be update
-				// a.delegate("update", a.file.Domain, record)
-				logrus.Infof("Update %v", a.file.Domain)
-				// }
+				if bs.Name != record.Name || bs.Value != a.DNSReverseProxyAddr || bs.Type != "A" { // if true, record exists and needs to be update
+					a.delegate("update", a.file.Domain, record)
+				}
 			}
 		}()
 		backoffWait(maxTries, leftTries, time.Minute) // wait time increases exponentially
@@ -83,21 +87,20 @@ func (a *Agent) Sync() {
 	}
 }
 
-// delegate appropriately calls the dns manager to handle the addition or removal of a DNS rule
-// func (a *Agent) delegate(action string, domain string, record file.Records) {
-// 	var ok bool
-// 	var err error
+// delegate appropriately calls the dns manager to handle the addition or update of a DNS rule
+func (a *Agent) delegate(action string, domain string, record file.Records) {
+	var err error
 
-// 	if action == "update" {
-// 		ok, err = a.WebhookClient.UpdateRecord(&hookTypes.DNSRecord{Name: record.Name, Type: record.Type, Value: record.Value})
-// 	}
+	if action == "update" {
+		err = a.WebhookClient.UpdateRecord(&hookTypes.DNSRecord{Name: record.Name, Type: record.Type, Value: record.Value})
+	}
 
-// 	if action == "create" {
-// 		ok, err = a.WebhookClient.AddRecord(record.Name, record.Type, record.Value) // adds to the dns manager
-// 	}
+	if action == "create" {
+		err = a.WebhookClient.AddRecord(record.Name, record.Type, record.Value)
+	}
 
-// 	if !ok {
-// 		logrus.Errorf("Error to %v the Domain '%v' from the service '%v': %v", action, domain, record.Name, err)
-// 	}
+	if err != nil {
+		logrus.Errorf("Error to %v the Domain '%v' from the service '%v': %v", action, domain, record.Name, err)
+	}
 
-// }
+}
